8467193cf6f2794f864d99f0d165e656fc944b4f
Alex Dehnert <alex@dehnerts.com>
2024-08-03 01:56:55 -0400

    Package Athena's aclocal macros

---
+++ aclocal.nix
@@ -0,0 +1,33 @@
+{ stdenv, pkgs, lib, fetchFromGitHub, pkg-config, }:
+
+let
+  fs = lib.fileset;
+in stdenv.mkDerivation {
+  pname = "debathena-aclocal";
+  version = "1.1.4";
+
+  src = fetchFromGitHub {
+    owner = "mit-athena";
+    repo = "aclocal";
+    rev = "1.1.4";
+    hash = "sha256-KGbEzxxi81fTFgNYtWX+z12PNgr36pZyNz2BVwGYfIo=";
+  };
+
+  buildPhase = ''
+  mkdir $out
+  mkdir $out/share
+  cp -r aclocal $out/share/aclocal
+  '';
+
+  meta = with lib; {
+    description = "Project Athena aclocal files";
+    homepage = "https://github.com/mit-athena/aclocal";
+    platforms = platforms.linux;
+  };
+
+  buildInputs = [
+    pkg-config
+  ];
+  nativeBuildInputs = [
+  ];
+}
