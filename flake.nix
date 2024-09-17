bb811903ff8af64d3f8996dacc41fa71586bb8a4
Alex Dehnert <alex@dehnerts.com>
2024-09-16 23:58:50 -0400

    nix: Update flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal NUR repository";
+  description = "MIT Athena packaging for Nix";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
