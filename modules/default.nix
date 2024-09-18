26ddd60cdaad94400d0a14229c70a37f1086a7c7
Alex Dehnert <alex@dehnerts.com>
2024-09-17 23:33:42 -0400

    meta: Create a "Nixathena Standard" "metapackage"
    Inspired by `debathena-standard`, this is certainly not complete, but is
    intended to be "the Nixathena you'd want on your laptop".

--- modules/default.nix
+++ modules/default.nix
@@ -1,8 +1,15 @@
 {
-  # Add your NixOS modules here
   # Don't forget that nixathena.modules.whatever also needs to be in the
   # `modules = [ ... ]` list of your config to actually process these.
+
+  # Enable the "standard" metapackage (clients, pyhesiodfs, etc.) by including
+  # the `nixathena.meta.standard` module and then setting
+  # `nixathena.meta.standard.enable = true;`
+
   discussd = ./discussd.nix;
   remctld = ./remctld.nix;
   pyhesiodfs = ./pyhesiodfs.nix;
+  meta = {
+    standard = ./meta/standard.nix;
+  };
 }
---
+++ modules/meta/standard.nix
@@ -0,0 +1,39 @@
+{ lib, pkgs, config, options, ...
+}:
+
+let
+  cfg = config.nixathena.meta.standard;
+  athena-pkgs = import ../../pkgs { inherit pkgs; };
+  defaultPackages = [
+    athena-pkgs.discuss
+    athena-pkgs.remctl
+  ];
+in
+{
+  imports = [
+    ../pyhesiodfs.nix
+  ];
+
+  options.nixathena.meta.standard = (let
+    mkOption = lib.mkOption;
+    mkEnableOption = lib.mkEnableOption;
+    types = lib.types;
+  in {
+    enable = mkEnableOption "Nixathena standard";
+    packages = mkOption {
+      description = "list of packages to install";
+      default = defaultPackages;
+      type = types.listOf types.package;
+    };
+  });
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = cfg.packages;
+    services.pyhesiodfs.enable = true;
+    security.krb5.enable = true;
+    services.openafsClient = {
+      enable = true;
+      cellName = "athena.mit.edu";
+    };
+  };
+}
