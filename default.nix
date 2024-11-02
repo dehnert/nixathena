ab6fde4940db42a6838ee70363c536acd6006775
Alex Dehnert <adehnert@mit.edu>
2024-11-02 13:20:25 -0400

    Add a reasonable `default` package
    Among other things, a plain `nix build` will now produce a *single*
    `result` symlink that incorporates many of the Nixathena packages, one
    per subdir.

--- default.nix
+++ default.nix
@@ -10,6 +10,8 @@
 
 let
   athena-pkgs = import ./pkgs { inherit pkgs; };
+  athena-python = with athena-pkgs; [ python-discuss python-afs python-hesiod locker-support ];
+  athena-python3 = (pkgs.python3.withPackages (ps: (map (pkg: pkg ps) athena-python)));
 in
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -22,6 +24,9 @@ in
   };
 
   inherit athena-pkgs;
+  # linkFarmFromDrvs is undocumented, but the source is at
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/trivial-builders/default.nix#L578
+  default = pkgs.linkFarmFromDrvs "nixathena-pkgs" (with athena-pkgs; [ discuss pyhesiodfs remctl athena-python3 ]);
   debathena-aclocal = athena-pkgs.debathena-aclocal;
   discuss = athena-pkgs.discuss;
   python-discuss = athena-pkgs.python-discuss;
@@ -29,11 +34,7 @@ in
   python-hesiod = athena-pkgs.python-hesiod;
   pyhesiodfs = athena-pkgs.pyhesiodfs;
   remctl = athena-pkgs.remctl;
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
-  athena-python3 = (pkgs.python3.withPackages (ps: (map (pkg: pkg ps) [
-    athena-pkgs.python-discuss
-    athena-pkgs.python-afs
-  ])));
+  athena-python3 = athena-python3;
 }
