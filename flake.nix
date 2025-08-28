5004f13b22c29cb755ac8bd415ae193e82df2ad4
Alex Dehnert <adehnert@mit.edu>
2025-08-28 00:57:35 -0400

    remctl: Fetch compatibly with restricted mode
    This fixes an error:
        error: access to URI
        'https://github.com/rra/remctl/commit/cad43fe0472db09b3f267f0c212d34a23360dc05.patch'
        is forbidden in restricted mode
    
    Apparently (https://github.com/NixOS/nix/issues/2043) builtins.fetchurl
    can't access the network in restricted mode (and part of the CI build
    runs in restricted mode for some reason?), but pkgs.fetchurl aka just
    fetchurl can.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, }:
     let
       systems = [
         "x86_64-linux"
--- pkgs/remctl.nix
+++ pkgs/remctl.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs, lib, fetchFromGitHub,
+{ stdenv, pkgs, lib, fetchurl, fetchFromGitHub,
   autoreconfHook, autoconf, automake, pkg-config,
   libkrb5, libevent, pcre2, systemdLibs,
   python3, perl,
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {
     # Required for automake 1.17+ compatibility
     # See https://github.com/rra/remctl/issues/35
     # This can probably be removed when updating beyond 3.18
-    (builtins.fetchurl {
+    (fetchurl {
       url = "https://github.com/rra/remctl/commit/cad43fe0472db09b3f267f0c212d34a23360dc05.patch";
       sha256 = "sha256:1l8s77h2w0ppiwa1rcbqgbwsh4jg0jpnjbm636a6qscgwmxy2saz";
     })
